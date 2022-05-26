3c9e7097a14b0d213eed07cac693b5c01bf82017
Anotherhour <95116711+Anotherhour@users.noreply.github.com>
2022-05-26 15:01:25 +0800

    Update Time.js

--- Time.js
+++ Time.js
@@ -1,35 +1,52 @@
-/* 参考 */
-/* https://raw.githubusercontent.com/fishingworld/something/main/PanelScripts/surgepro_reloadprofile.js */
-/* https://raw.githubusercontent.com/smartmimi/conf/master/surge/functionstatus.js */
-
+//代码借鉴 https://raw.githubusercontent.com/chaizia/Profiles/master/MySurge/surgepro_flushdns.js
+//https://raw.githubusercontent.com/smartmimi/conf/master/surge/functionstatus.js
 !(async () => {
-/* 定义图标 */
-let params = getParams($argument)
-/* 时间获取 */
-let traffic = (await httpAPI("/v1/traffic","GET"))
-let dateNow = new Date()
-let dateTime = Math.floor(traffic.startTime*1000)
-let startTime = timeTransform(dateNow,dateTime)
-/* MitM+Rewrite+Scripting状态获取 */
+let traffic = (await httpAPI("/v1/traffic","GET"));
+let dateNow = new Date();
+let dateTime = Math.floor(traffic.startTime*1000);
+let startTime = timeTransform(dateNow,dateTime);
 let mitm_status = (await httpAPI("/v1/features/mitm","GET"));
 let rewrite_status = (await httpAPI("/v1/features/rewrite","GET"));
 let scripting_status = (await httpAPI("/v1/features/scripting","GET"));
+let icon_s = mitm_status.enabled&&rewrite_status.enabled&&scripting_status.enabled;
+//点击按钮，刷新dns
+//if ($trigger == "button") await httpAPI("/v1/dns/flush");
+//点击按钮，重载配置（同时刷新dns）
 
-if ($trigger == "button") await httpAPI("/v1/profiles/reload");
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, 
+        "d+": this.getDate(), 
+        "H+": this.getHours(), 
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(), 
+        "q+": Math.floor((this.getMonth() + 3) / 3), 
+        "S": this.getMilliseconds() 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
 
-  $done({
-      title:"𝐒𝐮𝐫𝐠𝐞 𝐏𝐫𝐨 ",
-      content:`仿佛穿过林子便是海\n`+
-          "北京时间："+ (new Date()).Format("yyyy-MM-dd HH:mm:ss")+"\n
-	  `--------------\n`+
-	  `已持续运行: ${startTime}\n`+
-	  `MitM:`+icon_status(mitm_status.enabled)+`  Rewrite:`+icon_status(rewrite_status.enabled)+`  Scripting:`+icon_status(scripting_status.enabled),
-		icon: params.icon,
-		"icon-color":params.color
-    });
-
+if ($trigger == "button") {
+	await httpAPI("/v1/profiles/reload");
+	$notification.post("配置重载","配置重载成功","")
+};
+$done({
+    title:"𝐒𝐮𝐫𝐠𝐞 𝐏𝐫𝐨",
+    content: "北京时间："+ (new Date()).Format("yyyy-MM-dd HH:mm:ss")+"\n启动时长："+startTime + "\nMitm:"+icon_status(mitm_status.enabled)+"  仿佛穿过林子便是海 Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+    icon: icon_s?"crown.fill":"exclamationmark.triangle",
+   "icon-color":icon_s?"#EACD76":"#F20C00"
+});
 })();
-
+function icon_status(status){
+  if (status){
+    return "\u2611";
+  } else {
+      return "\u2612"
+    }
+}
 function timeTransform(dateNow,dateTime) {
 let dateDiff = dateNow - dateTime;
 let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
@@ -43,39 +60,19 @@ let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
 let seconds=Math.round(leave3/1000)
 
 if(days==0){
-
-	if(hours==0){
-	if(minutes==0)return(`${seconds}秒`);
-	return(`${minutes}分${seconds}秒`)
-	}
-	return(`${hours}时${minutes}分${seconds}秒`)
-	}else {
-	return(`${days}天${hours}时${minutes}分`)
-	}
-
-}
-
-function icon_status(status){
-  if (status){
-    return "\u2611";
-  } else {
-      return "\u2612"
+  if(hours==0){
+    if(minutes==0)return(`${seconds}秒`);
+      return(`${minutes}分${seconds}秒`)
     }
+    return(`${hours}时${minutes}分${seconds}秒`)
+  }else {
+        return(`${days}天${hours}时${minutes}分`)
+	}
 }
-
 function httpAPI(path = "", method = "POST", body = null) {
-    return new Promise((resolve) => {
-        $httpAPI(method, path, body, (result) => {
-            resolve(result);
-        });
+  return new Promise((resolve) => {
+    $httpAPI(method, path, body, (result) => {
+      resolve(result);
     });
-}
-
-function getParams(param) {
-  return Object.fromEntries(
-    $argument
-      .split("&")
-      .map((item) => item.split("="))
-      .map(([k, v]) => [k, decodeURIComponent(v)])
-  );
+  });
 }
